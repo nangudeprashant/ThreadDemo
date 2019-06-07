3ad863b36b8fdec3efa04a25cf705716de3b5198
Administrator <Administrator@DELL>
2019-06-07 18:59:57 +0530

    Thread synchronization and Thread joining codes are added.

--- ThreadDemo/src/main/java/com/javalive/ThreadDemo/BasicThreadExample.java
+++ ThreadDemo/src/main/java/com/javalive/ThreadDemo/BasicThreadExample.java
@@ -4,10 +4,40 @@ package com.javalive.ThreadDemo;
  * Hello world!
  *
  */
-public class BasicThreadExample 
+public class BasicThreadExample extends Thread
 {
+	public void run() {
+		System.out.println("In thread "+ Thread.currentThread().getName());
+		displayNumbers(11);
+	}
+	public void displayNumbers(int num){
+		System.out.println("In the method displayNumbers()");
+		for(int i=1;i<=10;i++)
+		  {
+			  System.out.println(num*i);
+		  try { Thread.sleep(500);
+		  
+		  }catch(Exception e){System.out.println(e);}
+		    
+		  }
+		  System.out.println("=============================================");
+	}
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	BasicThreadExample obj1=new BasicThreadExample();
+    	BasicThreadExample obj2=new BasicThreadExample();
+    	obj1.setName("MyThread1");
+    	System.out.println("First thread calling start() method");
+    	obj1.start();
+    	obj2.setName("MyThread2");
+    	System.out.println("Second thread calling start() method");
+    	obj2.start();
+    	System.out.println("=====================++++++++++++++++++++++++++++===============================================");
+    	/*System.out.println("First thread calling display() method");
+    	obj1.displayNumbers(11);
+    	System.out.println("\nSecond thread calling display() method");
+    	obj2.displayNumbers(5);*/
+    	
     }
+    
 }
--- ThreadDemo/src/main/java/com/javalive/ThreadDemo/ThreadJoinDemo.java
+++ ThreadDemo/src/main/java/com/javalive/ThreadDemo/ThreadJoinDemo.java
@@ -1,5 +1,115 @@
 package com.javalive.ThreadDemo;
 
-public class ThreadJoinDemo {
+class DataEntry {
+	public void enterData() {
+		System.out.println("Entering data to the system.......");
+	}
+}
 
+class DataValidation {
+	public void validateData() {
+		System.out.println("Validating data.......");
+	}
+}
+
+class DataSaving {
+	public void saveData() {
+		System.out.println("Saving data to the system.......");
+	}
+}
+
+class DataArchive {
+	public void archivData() {
+		System.out.println("Archiveing data.......");
+	}
+}
+
+class DataEntryThread implements Runnable {
+	private DataEntry dataEntryObject;
+
+	public DataEntryThread(DataEntry dataEntryObject) {
+		this.dataEntryObject = dataEntryObject;
+	}
+
+	public void run() {
+		dataEntryObject.enterData();
+	}
+}
+
+class DataValidationThread implements Runnable {
+	private DataValidation dataValidationObject;
+
+	public DataValidationThread(DataValidation dataValidationObject) {
+		this.dataValidationObject = dataValidationObject;
+	}
+
+	public void run() {
+		dataValidationObject.validateData();
+	}
+}
+
+class DataSavingThread implements Runnable {
+	private DataSaving dataSavingObject;
+
+	public DataSavingThread(DataSaving dataSavingObject) {
+		this.dataSavingObject = dataSavingObject;
+	}
+
+	public void run() {
+		dataSavingObject.saveData();
+	}
+}
+
+class DataArchiveThread implements Runnable {
+	private DataArchive dataArchiveObject;
+
+	public DataArchiveThread(DataArchive dataArchiveObject) {
+		this.dataArchiveObject = dataArchiveObject;
+	}
+
+	public void run() {
+		dataArchiveObject.archivData();
+	}
+}
+
+public class ThreadJoinDemo {
+	public static void main(String[] args) {
+		
+	DataEntry dataEntryObj = new DataEntry();
+	DataValidation dataValidationObj = new DataValidation();
+	DataSaving dataSavingObj = new DataSaving();
+	DataArchive dataArchivingObj=new DataArchive();
+	
+	DataEntryThread dataEntryThread=new DataEntryThread(dataEntryObj);
+	DataValidationThread dataValidationThread=new DataValidationThread(dataValidationObj);
+	DataSavingThread dataSavingThread=new DataSavingThread(dataSavingObj);
+	DataArchiveThread dataarchiveThread=new DataArchiveThread(dataArchivingObj);
+	
+	Thread thread1=new Thread(dataEntryThread);
+	thread1.start();
+	try {
+		thread1.join();
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	Thread thread2=new Thread(dataValidationThread);
+	thread2.start();
+	try {
+		thread2.join();
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	Thread thread3=new Thread(dataSavingThread);
+	thread3.start();
+	try {
+		thread3.join();
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	Thread thread4=new Thread(dataarchiveThread);
+	thread4.start();
+	}
 }
