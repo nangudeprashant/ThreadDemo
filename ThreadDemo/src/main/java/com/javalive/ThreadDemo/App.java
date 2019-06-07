f465214c1dccf75f664cd1ec3de023bd2b2925bc
Administrator <Administrator@DELL>
2019-06-07 14:56:56 +0530

    Initial commit.

---
+++ ThreadDemo/src/main/java/com/javalive/ThreadDemo/App.java
@@ -0,0 +1,13 @@
+package com.javalive.ThreadDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
